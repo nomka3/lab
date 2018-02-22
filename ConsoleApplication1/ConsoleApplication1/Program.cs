2fa1921f370c4a18b8d17bffd10adeb399adc651
unknown <Student@EM6.EMIS.local>
2018-02-22 17:22:45 +0700

    dfkghdkj

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lenght = 10, width = 5;
+            for (int j = 0; j < width; j++)
+            {
+                for (int i = 0; i < lenght; i++)
+                {
+                    if (j == 0 || j == width - 1) Console.Write("*");
+                    else if (i == 0 || i == lenght - 1) Console.Write("*");
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Action<int, int> write = (xp, yp) => { Console.SetCursorPosition(xp, yp); Console.Write("*"); };
+ 
+    int centerX = 20, centerY = 10, radius = 8, x = -radius;
+    while (x < radius)
+    {
+        var y = (int)Math.Floor(Math.Sqrt(radius * radius - x * x));
+ 
+        write(x + centerX, y + centerY);
+        y = -y;
+        write(x + centerX, y + centerY);
+        x++;
+    }
+    Console.ReadLine(); 
+            Console.ReadKey();
+}
+    
+     
+       }
+
+    }
--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication2
+﻿static void Main(string[] args)
 {
-    class Program
+    Action<int, int> write = (xp, yp) => { Console.SetCursorPosition(xp, yp); Console.Write("*"); };
+ 
+    int centerX = 20, centerY = 10, radius = 8, x = -radius;
+    while (x < radius)
     {
-        static void Main(string[] args)
-        {
-            int a;
-        }
-
+        var y = (int)Math.Floor(Math.Sqrt(radius * radius - x * x));
+ 
+        write(x + centerX, y + centerY);
+        y = -y;
+        write(x + centerX, y + centerY);
+        x++;
     }
+    Console.ReadLine();
 }
